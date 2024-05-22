aaf5d61c17091d16f441ef3e2827a0d0813eb1f1
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-05-22 14:39:54 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return 
+    
+    list = [0,1]
+    while len(list) <= n:
+        new_list = list[-1] + list[-2]
+        list.append(new_list)
+
+print(list[:n])
+    
